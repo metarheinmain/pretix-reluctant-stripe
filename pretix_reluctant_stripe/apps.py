75c3bafc07921e6e3003a4aeb7ae78b9cf17bcb5
Raphael Michel <michel@rami.io>
2022-12-17 18:12:44 +0100

    Stop using deprecated Django APIs

---
+++ pretix_reluctant_stripe/apps.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy
+from . import __version__
+
+
+class PluginApp(AppConfig):
+    name = 'pretix_reluctant_stripe'
+    verbose_name = 'Stripe plugin (reluctant to work)'
+
+    class PretixPluginMeta:
+        name = gettext_lazy('Stripe plugin (reluctant to work)')
+        author = 'Team MRMCD'
+        description = gettext_lazy('Short description')
+        visible = True
+        version = __version__
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.command.build import build
 
 from django.core import management
 from setuptools import setup, find_packages
+from pretix_reluctant_stripe import __version__
 
 
 try:
@@ -25,7 +26,7 @@ cmdclass = {
 
 setup(
     name='pretix-reluctant-stripe',
-    version='1.0.0',
+    version=__version__,
     description='Short description',
     long_description=long_description,
     url='https://github.com/metarheinmain/pretix-reluctant-stripe',
